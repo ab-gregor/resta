a92ac98b48a67fa3692d861cec685405d01ea8ec
Sagar R.B <sagarrb1805@gmail.com>
2023-04-10 19:21:05 +0530

    issue with displaying cart items.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,20 +23,31 @@ function Cart() {
       payload: data
     })
   }
-  let itemVal;
+  let itemVal=[];
   const menuItems =  menuList.menu
-  console.log(menuItems)
-  cartItems.map((cartItem)=>(
-    itemVal =menuItems?.filter((item)=>{
-        return item.id === cartItem.itemid
-      })
-))
 
+//   console.log(menuItems)
+
+  cartItems.map((cartItem)=>(
+    itemVal.push(menuItems?.filter((item)=>{
+        return item.id === cartItem.itemid
+      }))
+))
+    //   console.log(itemVal)
+
+    
 
   return (
     <div>
         <h1>Cart</h1>
-        {/* <h2>{itemVal}</h2> */}
+        {itemVal.map((item)=>
+            
+            {item.map((ite)=>(
+                console.log(ite.itemName)
+                // <h2>{ite.itemName}</h2>
+                // <p>{ite.itemName}</p>
+        ))}
+        )}
     </div>
   )
 }
